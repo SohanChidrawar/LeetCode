bb27d8fb5ce0bc5906103ca5e67e38bda654a911
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2024-07-22 16:55:14 +0530

    Create optimised_tabulation.py

---
+++ Knapsack with Duplicate Items/optimised_tabulation.py
@@ -0,0 +1,15 @@
+    def knapSack(self, N, W, val, wt):
+        # code here
+        
+        prev = [0]*(W+1)
+        curr = [0]*(W+1)
+        
+        for i in range(1,N+1):
+            for j in range(1,W+1):
+                exclude = prev[j]
+                include = 0
+                if wt[i-1]<=j:
+                    include = val[i-1] + curr[j-wt[i-1]]
+                curr[j] = max(exclude,include)
+            prev = curr[:]
+        return curr[W]
