5a7cfaa07a78259e434ab0a6b6a10b40d5dadc6a
Sohan Chidrawar <99244757+SohanChidrawar@users.noreply.github.com>
2024-07-23 17:25:57 +0530

    Create memoisation.py

---
+++ 72. Edit Distance/memoisation.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+        dp = [[-1]*(m) for _ in range(n)]
+
+        def helper(index1, index2):
+            #base case
+            if index1>n-1 and index2>m-1:
+                return 0
+
+            if index1>n-1:
+                return m-index2
+                
+            if index2>m-1:
+                return n-index1
+
+            if dp[index1][index2]!=-1:
+                return dp[index1][index2]
+            #recursive case
+            if word1[index1] == word2[index2]:
+                dp[index1][index2] = helper(index1+1, index2+1)
+            else:
+                replace = 1 + helper(index1+1, index2+1)
+                delete = 1 + helper(index1+1, index2)
+                insert = 1 + helper(index1, index2+1)
+                dp[index1][index2] = min(replace, delete, insert)
+            return dp[index1][index2]
+        
+        return helper(0,0)
